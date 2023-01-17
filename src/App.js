821b8ca527bb692555ea31fa3b9963d3cb057bb2
nurmatjon_abdullayev <anstronnna@gmail.com>
2023-01-17 16:01:31 +0500

    add basic routing

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.scss';
 import About from './components/About/About';
 import Contact from './components/Contact/Contact';
@@ -6,11 +7,15 @@ import Header from './components/Header/Header';
 
 function App() {
   return (
+
     <div className='wrapper'>
       <Header />
       <main className='main'>
-        <About />
-        <Contact/>
+        <Routes>
+          <Route path='/about' element={<About />} />
+          <Route path='/contacts' element={ <Contact/>} />
+        </Routes>
+        
       </main>
       <Footer />
 
