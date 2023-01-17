58573137ea27de9ad26fe54b27d0c9cdf1078ddf
nurmatjon_abdullayev <anstronnna@gmail.com>
2023-01-17 16:46:50 +0500

    add basic routing

---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Layout() {
+  return (
+    <div className="wrapper">
+      <Header />
+      <main className="main"></main>
+      <Footer />
+    </div>
+  );
+}
