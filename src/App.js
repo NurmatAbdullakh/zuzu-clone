7f0348477f7c8d0d3500086115da1b97a26a09be
nurmatjon_abdullayev <anstronnna@gmail.com>
2023-01-18 14:44:07 +0500

    create branches

--- src/App.js
+++ src/App.js
@@ -1,20 +1,54 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.scss";
 import About from "./components/About/About";
+import Branches from "./components/Branchs/Branchs";
 import Contact from "./components/Contact/Contact";
 import Home from "./components/Home/Home";
 import Layout from "./components/Layout/Layout";
 import NotFoundPage from "./components/NotFoundPage/NotFoundPage";
+const routes = [
+  {
+    path: "/",
+    element: <Layout />,
+    childs: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "branches",
+        element: <Branches />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+      {
+        path: "contacts",
+        element: <Contact />,
+      },
+      {
+        path: "*",
+        element: <NotFoundPage />,
+      },
+    ],
+  },
+];
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="about" element={<About />} />
-        <Route path="contacts" element={<Contact />} />
-        <Route path="*" element={<NotFoundPage />} />
-      </Route>
+      {routes.map((v) => (
+        <Route path={v.path} element={v.element}>
+          {v.childs.map((e) =>
+            e.path === v.path ? (
+              <Route index element={e.element} />
+            ) : (
+              <Route path={e.path} element={e.element} />
+            )
+          )}
+        </Route>
+      ))}
     </Routes>
   );
 }
---
+++ src/components/Branchs/Branchs.jsx
@@ -0,0 +1,5 @@
+import styles from "./Branchs.module.scss";
+
+export default function Branches() {
+  return <div></div>;
+}
