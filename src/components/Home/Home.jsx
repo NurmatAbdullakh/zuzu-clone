096665c2da71805100369b038ddda45fe3eca9bf
nurmatjon_abdullayev <anstronnna@gmail.com>
2023-01-24 15:43:43 +0500

    get products from fake store api

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,13 @@
 import Modal from "@mui/material/Modal";
-import { useState } from "react";
-import { useSelector } from "react-redux";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setData } from "../../redux/productsReducer";
 import Order from "../Order/Order";
 import Products from "../Products/Products";
 
 export default function Home() {
+  const dispatch = useDispatch();
   const categories = useSelector((state) => state.products.data);
   const [currentProduct, setCurrentProduct] = useState({});
   const [open, setOpen] = useState(false);
@@ -17,9 +20,38 @@ export default function Home() {
     handleOpen();
   };
 
+  useEffect(() => {
+    axios
+      .get("https://fakestoreapi.com/products")
+      .then((response) => {
+        // console.log("then=>>>", response.data);
+        dispatch(setData(response.data));
+      })
+      .catch((error) => {
+        console.log("catch=>>>>", error);
+      })
+      .finally(() => {
+        console.log("finally=>>>");
+      });
+  }, []);
+  const formatCategories = (arr) => {
+    const categories = arr.reduce(
+      (total, v, i, array) =>
+        total.includes(v.category) ? total : [...total, v.category],
+      []
+    );
+    return categories.reduce((total, category) => {
+      return [
+        ...total,
+        { category, products: arr.filter((e) => e.category === category) },
+      ];
+    }, []);
+  };
+  const formattedCategories = formatCategories(categories);
+  console.log("formattedCategories===>>>", formattedCategories);
   return (
     <>
-      {categories.map((v) => (
+      {formattedCategories.map((v) => (
         <Products
           onButtonClick={onProductButtonClick}
           category={v.category}
